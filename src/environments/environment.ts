149376fc67766edc296c6f9cc94feb5b41b2d520
erik-gustafsson <erikephggustafsson@gmail.com>
2023-02-01 09:03:20 +0100

    Added enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = { 
+    production: false,
+    apiTrainers: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers ",
+    apiPokemons: "",
+    apiKey: "EZRe795eHLuIZXbpnPTtYHPHdmTe5zkNLvmK2q3bxqox1COidDC3gmJSC0chTXF2"
+};
