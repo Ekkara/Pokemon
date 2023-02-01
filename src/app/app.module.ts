30b27fa5a61796786398dee11f2ef7847efecde9
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-01 10:27:21 +0200

    feat: basic structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    PokemonCataloguePage
   ],
   imports: [
     BrowserModule
