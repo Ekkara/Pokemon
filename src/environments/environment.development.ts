2e09817c6d4e7f52ab5986081351618348f4888f
erik-gustafsson <erikephggustafsson@gmail.com>
2023-02-06 21:58:36 +0100

    deployed keys

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {    
+    production: false,
+    apiTrainers: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers",
+    apiFavorites: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers?id=",
+    apiKey: "EZRe795eHLuIZXbpnPTtYHPHdmTe5zkNLvmK2q3bxqox1COidDC3gmJSC0chTXF2"
+};
