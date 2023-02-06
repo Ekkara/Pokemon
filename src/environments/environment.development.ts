fd60c286dc736741ac633a819928b8d259d454dc
erik-gustafsson <erikephggustafsson@gmail.com>
2023-02-06 08:31:53 +0100

    Patch
    hide the api-key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,6 @@
-export const environment = {
-    
+export const environment = {    
     production: false,
     apiTrainers: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers",
-    apiPokemons: "",
-    apiKey: "EZRe795eHLuIZXbpnPTtYHPHdmTe5zkNLvmK2q3bxqox1COidDC3gmJSC0chTXF2",
-    apiFavorites: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers?id="
-
+    apiFavorites: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers?id=",
+    apiKey: ""
 };
