187a6c62b85da374bb11253677b9018f4ac43076
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-06 22:01:19 +0200

    doc: gitignore

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = { 
-    production: false,
-    apiTrainers: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers",
-    apiFavorites: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers?username=",
-    apiKey: ""
-};
