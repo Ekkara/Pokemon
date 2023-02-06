fdab8c27a5a29e3fb0b2fd4d5db41247b09cbc88
erik-gustafsson <erikephggustafsson@gmail.com>
2023-02-06 18:18:56 +0100

    patch
    removed apikey "EZRe795eHLuIZXbpnPTtYHPHdmTe5zkNLvmK2q3bxqox1COidDC3gmJSC0chTXF2"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiTrainers: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers",
     apiFavorites: "https://noroff-assignment-api-production-fa70.up.railway.app/trainers?username=",
-    apiKey: "EZRe795eHLuIZXbpnPTtYHPHdmTe5zkNLvmK2q3bxqox1COidDC3gmJSC0chTXF2"
+    apiKey: ""
 };
